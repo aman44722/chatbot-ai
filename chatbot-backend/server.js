01c8c0c6a9cdbe12d808da52861a257968125752
aman singh <aman44722@gmail.com>
2026-05-23 15:43:49 +0530

    fix: remove compression (Express 5 incompatibility)

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const compression = require("compression");
 const authRoutes = require('./routes/authRoutes');
 const conversationRoutes = require('./routes/conversationRoutes');
 const botRoutes = require('./routes/botRoutes');
@@ -33,7 +32,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(compression());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
 
